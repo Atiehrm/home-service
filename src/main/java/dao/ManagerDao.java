919b2693db1e94a7e25592003b02639caa6a9bd1
Atieh <atieh.rahbari@gmail.com>
2022-01-05 23:20:10 +0330

    add save method in ManagerDao in dao package

--- src/main/java/dao/ManagerDao.java
+++ src/main/java/dao/ManagerDao.java
@@ -1,7 +1,20 @@
 package dao;
 
+import config.HibernateUtil;
+import model.entity.member.Manager;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  * @author arm 1/5/2022
  */
 public class ManagerDao {
+
+    public void save(Manager manager) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(manager);
+        transaction.commit();
+        session.close();
+    }
 }
