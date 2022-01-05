072b5682c372ab6b44f7d7cd963abedfdd60188f
Atieh <atieh.rahbari@gmail.com>
2022-01-05 23:27:51 +0330

    add save, findByEmail,findById methods in CustomerDao in dao package

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,8 +1,43 @@
 package dao;
 
+import config.HibernateUtil;
+import model.entity.member.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
+
 /**
  * @author arm 1/5/2022
  */
 public class CustomerDao {
 
+    public void save(Customer customer) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+        transaction.commit();
+        session.close();
+    }
+
+    public Optional<Customer> findByEmail(String email) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Customer> query = session.createQuery("FROM Customer customer WHERE customer.email=:email");
+        query.setParameter("email", email);
+        Optional<Customer> customer = Optional.ofNullable(query.uniqueResult());
+        transaction.commit();
+        session.close();
+        return customer;
+    }
+
+    public Customer findById(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Customer customer = session.get(Customer.class, id);
+        transaction.commit();
+        session.close();
+        return customer;
+    }
 }
