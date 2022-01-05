68da4a56b25946466cb170df3c9d8c2b2fe62f29
Atieh <atieh.rahbari@gmail.com>
2022-01-05 22:33:03 +0330

    add save, read,delete method in UserDao.java

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -9,7 +9,29 @@ import org.hibernate.Transaction;
  * @author arm 1/5/2022
  */
 public class UserDao {
+    public void save(User user) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+        session.close();
+    }
+    public User read(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User foundedUser = session.get(User.class, id);
+        transaction.commit();
+        session.close();
+        return foundedUser;
+    }
 
+    public void delete(User user) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(user);
+        transaction.commit();
+        session.close();
+    }
 
 
 }
