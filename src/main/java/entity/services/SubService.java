0d1bde41facea306bbd07ae5e0332657a4e8e244
Atieh <atieh.rahbari@gmail.com>
2022-01-05 20:25:25 +0330

    create SubService entity

--- src/main/java/entity/services/SubService.java
+++ src/main/java/entity/services/SubService.java
@@ -1,8 +1,28 @@
 package entity.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
 /**
  * @author arm 1/5/2022
  */
-public class SubService
-{
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class SubService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String name;
+    @ManyToOne
+    private Service service;
+    private long price;
+    private String description;
 }
