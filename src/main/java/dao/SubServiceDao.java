7e37ed99001a093a5095c0b5043e1176fb803da7
Atieh <atieh.rahbari@gmail.com>
2022-01-05 23:54:24 +0330

    add crud and findByName and findAll methods in SubServiceDao in dao package

---
+++ src/main/java/dao/SubServiceDao.java
@@ -0,0 +1,71 @@
+package dao;
+
+import config.HibernateUtil;
+import model.entity.services.SubService;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author arm
+ */
+public class SubServiceDao {
+
+    public void save(SubService subService) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(subService);
+        transaction.commit();
+        session.close();
+    }
+
+    public SubService read(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        SubService subService = session.get(SubService.class, id);
+        transaction.commit();
+        session.close();
+        return subService;
+    }
+
+    public void update(SubService subService) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(subService);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(SubService subService) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(subService);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<SubService> findAll() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<SubService> query = session.createQuery("FROM SubService");
+        List<SubService> subServices = query.list();
+        transaction.commit();
+        session.close();
+        return subServices;
+    }
+
+    public Optional<SubService> findByName(String name) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "from SubService subservice where subservice.name=:name";
+        Query<SubService> query = session.createQuery(hql, SubService.class);
+        query.setParameter("name", name);
+        Optional<SubService> subService = Optional.ofNullable(query.uniqueResult());
+        transaction.commit();
+        session.close();
+        return subService;
+    }
+}
