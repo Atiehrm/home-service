b8bcee32d3f7bc7a776460eb141f0e41505c5ed6
Atieh <atieh.rahbari@gmail.com>
2022-01-05 22:36:39 +0330

    add getAllUsers method in UserDao.java

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -4,6 +4,9 @@ import config.HibernateUtil;
 import model.entity.member.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 /**
  * @author arm 1/5/2022
@@ -16,6 +19,7 @@ public class UserDao {
         transaction.commit();
         session.close();
     }
+
     public User read(int id) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
@@ -33,5 +37,13 @@ public class UserDao {
         session.close();
     }
 
-
+    public List<User> getAllUsers() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from  User ");
+        List<User> userList = query.list();
+        transaction.commit();
+        session.close();
+        return userList;
+    }
 }
