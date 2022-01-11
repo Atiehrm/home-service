68e057971132cf9dd1d29d1ff8c8d55b411c6a0b
Atieh <atieh.rahbari@gmail.com>
2022-01-12 00:25:53 +0330

    delete HibernateUtil

--- src/main/java/ir/maktab/data/dao/WorkSuggestionRepository.java
+++ src/main/java/ir/maktab/data/dao/WorkSuggestionRepository.java
@@ -1,9 +1,6 @@
 package ir.maktab.data.dao;
 
-import ir.maktab.util.HibernateUtil;
 import ir.maktab.data.model.entity.WorkSuggestion;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +8,6 @@ import org.springframework.stereotype.Repository;
  * @author arm
  */
 @Repository
-public interface WorkSuggestionRepository extends CrudRepository<WorkSuggestion,Integer> {
+public interface WorkSuggestionRepository extends CrudRepository<WorkSuggestion, Integer> {
 
 }
