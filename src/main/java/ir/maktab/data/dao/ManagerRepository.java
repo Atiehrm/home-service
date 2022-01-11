569ce0c75a671811b944a4bf9be028cf4ad4a45f
Atieh <atieh.rahbari@gmail.com>
2022-01-11 23:53:53 +0330

    change ManagerDao to ManagerRepository interface

---
+++ src/main/java/ir/maktab/data/dao/ManagerRepository.java
@@ -0,0 +1,15 @@
+package ir.maktab.data.dao;
+
+import ir.maktab.data.model.entity.member.Manager;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * @author arm 1/5/2022
+ */
+@Repository
+public interface ManagerRepository extends CrudRepository<Manager, Integer> {
+    Optional<Manager> findByEmail(String email);
+}
